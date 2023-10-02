faad3fef3609ab9062861635ea11f24c6fe140f1
Adekoya Daniel <adekoyadaniel53@gmail.com>
2023-10-02 20:02:05 +0100

    chore: signup page

--- app/screens/Signup.tsx
+++ app/screens/Signup.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import AuthLayouts from '../layouts/AuthLayout';
 import AppText from '../components/AppText';
+import AppInput from '../components/shared/AppInput';
+import AppButton from '../components/shared/AppButton';
 
 export default function SignUpScreen({navigation}) {
   return (
     <AuthLayouts title='Sign up' desc="Your Very Own Virtual Assistant">
+         <AppInput label='Email Address' placeholder='Type in Email Address' />
+        <AppInput label='Password' placeholder='Type in Password' secureTextEntry={true} />
+        <AppInput label='Confirm Password' placeholder='Type in Password' secureTextEntry={true} />
+        <AppButton title='Sign up' onPress={() => {
+navigation.navigate('Main')
+        }} />
    
     <View>
         <AppText>Already have an account? <AppText onPress={() => {
