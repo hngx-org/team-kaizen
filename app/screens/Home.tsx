70895bf69d74239a2b91f6e8f00ed68f704edb4b
Adekoya Daniel <adekoyadaniel53@gmail.com>
2023-10-06 20:57:29 +0100

    CHORE: assistant

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Alert, ScrollView, StyleSheet, View} from 'react-native';
+import {
+  Alert,
+  Linking,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import AppText from '../components/AppText';
 import DashboardLayout from '../layouts/DashboardLayout';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -11,9 +18,11 @@ function HomeScreen({navigation}): JSX.Element {
   const [loading, setLoading] = useState();
   const [response, setResponse] = useState();
   const [error, setError] = useState();
+  const [open, setOpen] = useState(false);
 
   const [inputData, setInputData] = useState('');
   const [history, setHistory] = useState([]);
+  const [history2, setHistory2] = useState([]);
 
   const newRes = response;
   const newInput = inputData;
@@ -65,111 +74,337 @@ function HomeScreen({navigation}): JSX.Element {
   return (
     <>
       <View
-        style={{backgroundColor: 'white', padding: 20, left: 0, width: '100%'}}>
-        <AppInput
-          setText={val => {
-            setInputData(val);
-          }}
-          placeholder="what would you like me to do for you"
-        />
-        <AppButton
-          title="Ask"
-          disabled={loading}
-          style={{
-            marginTop: 0,
-          }}
+        style={{
+          justifyContent: 'space-between',
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          backgroundColor: '#fff',
+          flexDirection: 'row',
+          width: '100%',
+        }}>
+        <TouchableOpacity
+          style={{width: '48%'}}
           onPress={() => {
-            chatWithAi();
-          }}
-        />
+            setOpen(false);
+          }}>
+          <View
+            style={{
+              marginTop: 0,
+              width: '100%',
+              backgroundColor: open ? '#ccccccdf' : '#98c28ddf',
+              padding: 20,
+              borderRadius: 10,
+            }}>
+            <AppText
+              style={{
+                color: '#fff',
+                textAlign: 'center',
+              }}>
+              Use Assistant
+            </AppText>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{width: '48%'}}
+          onPress={() => {
+            setOpen(true);
+          }}>
+          <View
+            style={{
+              marginTop: 0,
+              width: '100%',
+              backgroundColor: open ? '#98c28ddf' : '#ccccccdf',
+              borderRadius: 10,
+              padding: 20,
+            }}>
+            <AppText
+              style={{
+                textAlign: 'center',
+                color: '#fff',
+              }}>
+              Chat With AI
+            </AppText>
+          </View>
+        </TouchableOpacity>
       </View>
-      <DashboardLayout>
-        {history?.length === 0 && (
-          <AppText style={styles.empty}>What whould you like to do</AppText>
-        )}
-        {history?.map((item, index) => {
-          return item.split(' ').includes('user:') ||
-            item.split(' ').includes('error:') ? (
-            <View key={index} style={{flexDirection: 'row', marginTop: 10}}>
-              <View
-                style={{
-                  height: 40,
-                  width: 40,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: item.split(' ').includes('error:')
-                    ? 'red'
-                    : '#8c8c8ce3',
-                  borderRadius: 1000,
-                  marginRight: 10,
-                }}>
-                <AppText style={{color: '#fff'}}>
-                  {item.split(' ').includes('error:') ? '' : 'M E'}
-                </AppText>
+      {open ? (
+        <>
+          <View
+            style={{
+              backgroundColor: 'white',
+              padding: 20,
+              left: 0,
+              width: '100%',
+            }}>
+            <AppInput
+              setText={val => {
+                setInputData(val);
+              }}
+              placeholder="what would you like me to do for you"
+            />
+            <AppButton
+              title="Ask"
+              disabled={loading}
+              style={{
+                marginTop: 0,
+              }}
+              onPress={() => {
+                chatWithAi();
+              }}
+            />
+          </View>
+          <DashboardLayout>
+            {history?.length === 0 && (
+              <AppText style={styles.empty}>What whould you like to do</AppText>
+            )}
+            {history?.map((item, index) => {
+              return item.split(' ').includes('user:') ||
+                item.split(' ').includes('error:') ? (
+                <View key={index} style={{flexDirection: 'row', marginTop: 10}}>
+                  <View
+                    style={{
+                      height: 40,
+                      width: 40,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      backgroundColor: item.split(' ').includes('error:')
+                        ? 'red'
+                        : '#8c8c8ce3',
+                      borderRadius: 1000,
+                      marginRight: 10,
+                    }}>
+                    <AppText style={{color: '#fff'}}>
+                      {item.split(' ').includes('error:') ? '' : 'M E'}
+                    </AppText>
+                  </View>
+                  <AppText style={styles.person(item)}>
+                    {item.split(' ').includes('error:')
+                      ? item.replace('error:', '-')
+                      : item.replace('user:', '-')}
+                  </AppText>
+                </View>
+              ) : (
+                <View
+                  key={index}
+                  style={{flexDirection: 'row', marginLeft: 'auto'}}>
+                  <AppText style={styles.ai(item)}>
+                    {item.replace('AI:', '-')}
+                  </AppText>
+                  <View
+                    style={{
+                      height: 40,
+                      width: 40,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      backgroundColor: '#000000e2',
+                      borderRadius: 1000,
+                      marginLeft: 10,
+                    }}>
+                    <AppText style={{color: '#fff'}}>A I</AppText>
+                  </View>
+                </View>
+              );
+            })}
+            {error && (
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <View
+                  style={{
+                    height: 40,
+                    width: 40,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#ff0000df',
+                    borderRadius: 1000,
+                    marginRight: 10,
+                  }}>
+                  <AppText style={{color: '#fff'}}></AppText>
+                </View>
+                <AppText style={styles.error}>{error}</AppText>
               </View>
-              <AppText style={styles.person(item)}>
-                {item.split(' ').includes('error:')
-                  ? item.replace('error:', '-')
-                  : item.replace('user:', '-')}
-              </AppText>
-            </View>
-          ) : (
-            <View
-              key={index}
-              style={{flexDirection: 'row', marginLeft: 'auto'}}>
-              <AppText style={styles.ai(item)}>
-                {item.replace('AI:', '-')}
-              </AppText>
-              <View
-                style={{
-                  height: 40,
-                  width: 40,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  backgroundColor: '#000000e2',
-                  borderRadius: 1000,
-                  marginLeft: 10,
-                }}>
-                <AppText style={{color: '#fff'}}>A I</AppText>
+            )}
+            {loading && (
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <View
+                  style={{
+                    height: 40,
+                    width: 40,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#525252e0',
+                    borderRadius: 1000,
+                    marginRight: 10,
+                  }}>
+                  <AppText style={{color: '#fff'}}></AppText>
+                </View>
+                <AppText style={styles.loading}>Typing...</AppText>
               </View>
-            </View>
-          );
-        })}
-        {error && (
-          <View style={{flexDirection: 'row', marginTop: 10}}>
+            )}
+          </DashboardLayout>
+        </>
+      ) : (
+        <>
+          <View
+            style={{
+              paddingHorizontal: 20,
+              backgroundColor: '#fff',
+            }}>
+            <AppText
+              style={{
+                marginTop: 20,
+              }}>
+              Basic Prompts
+            </AppText>
+
             <View
               style={{
-                height: 40,
-                width: 40,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#ff0000df',
-                borderRadius: 1000,
-                marginRight: 10,
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'space-between',
               }}>
-              <AppText style={{color: '#fff'}}></AppText>
+              <AppText
+                onPress={() => {
+                  setHistory2(current => [
+                    ...current,
+                    `user: Generate Google Meet Link`,
+                    `AI:https://meet.google.com/new`,
+                  ]);
+                }}
+                style={styles.prompt}>
+                Generate Google Meet Link
+              </AppText>
+              <AppText
+                onPress={() => {
+                  setHistory2(current => [
+                    ...current,
+                    `user: What's today's current Date?`,
+                    `AI2: ${new Date()}`,
+                  ]);
+                }}
+                style={styles.prompt}>
+                Ask for Current Date
+              </AppText>
             </View>
-            <AppText style={styles.error}>{error}</AppText>
           </View>
-        )}
-        {loading && (
-          <View style={{flexDirection: 'row', marginTop: 10}}>
-            <View
-              style={{
-                height: 40,
-                width: 40,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#525252e0',
-                borderRadius: 1000,
-                marginRight: 10,
-              }}>
-              <AppText style={{color: '#fff'}}></AppText>
-            </View>
-            <AppText style={styles.loading}>Typing...</AppText>
-          </View>
-        )}
-      </DashboardLayout>
+          <DashboardLayout>
+            {history2?.length === 0 && (
+              <AppText style={styles.empty}>What whould you like to do</AppText>
+            )}
+            {history2?.map((item, index) => {
+              return item.split(' ').includes('user:') ||
+                item.split(' ').includes('error:') ? (
+                <View key={index} style={{flexDirection: 'row', marginTop: 10}}>
+                  <View
+                    style={{
+                      height: 40,
+                      width: 40,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      backgroundColor: item.split(' ').includes('error:')
+                        ? 'red'
+                        : '#8c8c8ce3',
+                      borderRadius: 1000,
+                      marginRight: 10,
+                    }}>
+                    <AppText style={{color: '#fff'}}>
+                      {item.split(' ').includes('error:') ? '' : 'M E'}
+                    </AppText>
+                  </View>
+                  <AppText style={styles.person(item)}>
+                    {item.replace('user:', '-')}
+                  </AppText>
+                </View>
+              ) : (
+                <View
+                  key={index}
+                  style={{flexDirection: 'row', marginLeft: 'auto'}}>
+                  <AppText
+                    onPress={() => {
+                      if (item?.split(' ').includes('AI2:')) {
+                        if (Platform.OS === 'ios') {
+                          Linking.openURL('calshow:');
+                        } else if (Platform.OS === 'android') {
+                          Linking.openURL(
+                            'content://com.android.calendar/time/',
+                          );
+                        }
+                      } else {
+                        Linking.openURL(item.replace('AI:', ''));
+                      }
+                    }}
+                    style={styles.ai(item)}>
+                    {item?.split(' ').includes('AI2:') ? (
+                      <>
+                        <AppText style={{color: 'white'}}>
+                          Todays Date is
+                        </AppText>
+                        <AppText style={{color: '#1658ff'}}>
+                          {item.replace('AI2:', ' - ')}
+                        </AppText>
+                      </>
+                    ) : (
+                      <>
+                        <AppText style={{color: 'white'}}>
+                          Here is your link
+                        </AppText>
+                        <AppText style={{color: '#1658ff'}}>
+                          {item.replace('AI:', ' - ')}
+                        </AppText>
+                      </>
+                    )}
+                  </AppText>
+                  <View
+                    style={{
+                      height: 40,
+                      width: 40,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      backgroundColor: '#000000e2',
+                      borderRadius: 1000,
+                      marginLeft: 10,
+                    }}>
+                    <AppText style={{color: '#fff'}}>A I</AppText>
+                  </View>
+                </View>
+              );
+            })}
+            {error && (
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <View
+                  style={{
+                    height: 40,
+                    width: 40,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#ff0000df',
+                    borderRadius: 1000,
+                    marginRight: 10,
+                  }}>
+                  <AppText style={{color: '#fff'}}></AppText>
+                </View>
+                <AppText style={styles.error}>{error}</AppText>
+              </View>
+            )}
+            {loading && (
+              <View style={{flexDirection: 'row', marginTop: 10}}>
+                <View
+                  style={{
+                    height: 40,
+                    width: 40,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#525252e0',
+                    borderRadius: 1000,
+                    marginRight: 10,
+                  }}>
+                  <AppText style={{color: '#fff'}}></AppText>
+                </View>
+                <AppText style={styles.loading}>Typing...</AppText>
+              </View>
+            )}
+          </DashboardLayout>
+        </>
+      )}
     </>
   );
 }
@@ -213,6 +448,18 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     opacity: 0.6,
     marginTop: 40,
+    marginBottom: 10,
+  },
+  prompt: {
+    backgroundColor: '#bdbdbde1',
+    color: '#000000',
+    padding: 12,
+    borderRadius: 10,
+    opacity: 0.6,
+    textAlign: 'center',
+    width: '48%',
+    fontSize: 12,
+    marginTop: 10,
     marginBottom: 10,
   },
 
